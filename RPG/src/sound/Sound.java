e9d4c2fae1d6cff38d1c7aa1cb431f0948402f48
Yukine24 <35545363+Yukine24@users.noreply.github.com>
2018-03-21 09:23:30 +0100

    Add files via upload

---
+++ RPG/src/sound/Sound.java
@@ -0,0 +1,35 @@
+package sound;
+
+import java.io.*;
+import java.net.URL;
+import javax.sound.sampled.*;
+import javax.swing.*;
+
+
+// To play sound using Clip, the process need to be alive.
+// Hence, we use a Swing application.
+public class Sound {
+   
+   // Constructor
+   public Sound() {
+   
+      try {
+         // Open an audio input stream.
+    	  File soundFile = new File("blackpanther.wav");
+    	  AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+         // Get a sound clip resource.
+         Clip clip = AudioSystem.getClip();
+         // Open audio clip and load samples from the audio input stream.
+         clip.open(audioIn);
+         clip.start();
+         clip.loop(Clip.LOOP_CONTINUOUSLY);
+      } catch (UnsupportedAudioFileException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (LineUnavailableException e) {
+         e.printStackTrace();
+      }
+   }
+   
+}
