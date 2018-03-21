5d5d67ac404de50db03d3ca39a6cd9cb64fb166a
Smerilys <smerilys@gmail.com>
2018-03-21 11:25:29 +0100

    updated Game class

--- RPG/src/gameEngine/Game.java
+++ RPG/src/gameEngine/Game.java
@@ -3,12 +3,65 @@ package gameEngine;
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
+import gameDisplay.*;
+import dataClasses.*;
+import players.*;
+import java.awt.event.*;
+import java.awt.event.KeyEvent; 
 
-public class Game {
-	JFrame game = new JFrame();
-	public Map map = new Map();
+public class Game extends Thread {
 	
-	public Game(){
+	
+	// Both of these should only be initialized once (see : Singleton Pattern)
+	public Map map = new Map();
+	public User player = new User();
+	
+	// Create the panel containing the actual game
+	private RoomDisplay displayer = new RoomDisplay(map, player);
+	
+	private boolean quit = false;
+	
+	public Game() {
+		// Window of the whole Game (including HUD and Playable part)
+		JFrame gameWindow = new JFrame();
 		
+		// Debug : print the layout of each room
+		map.printMap(map);
+		
+		// Initialize the basic parameters
+		gameWindow.setVisible(true);
+		gameWindow.setSize(1280, 960);
+		gameWindow.setDefaultCloseOperation(gameWindow.EXIT_ON_CLOSE);
+		gameWindow.setResizable(true);
+		gameWindow.setLocationRelativeTo(null);
+		gameWindow.setTitle("RPG");
+		
+		// Set up the HUD
+		HUD headUpDisplay = new HUD(gameWindow);
+		
+		// Displayer is used to draw every time it is possible
+		gameWindow.setContentPane(displayer);
+		
+		// Game keeps running until the user quits
+		while(!quit)
+		{
+			// Caution : May loop forever - for now, stop with xkill command
+			displayRoom();
+		}
+		System.exit(0);
+	}
+	
+	public void displayRoom()
+	{
+		try {
+			Thread.sleep(1000);
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		// Update the window of the game
+		displayer.removeAll();
+		displayer.revalidate();
+		displayer.repaint();
 	}
 }
