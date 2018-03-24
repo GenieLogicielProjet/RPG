e7113b35e00aa7de9a3c1bdf1520d7f3b100d77d
Smerilys <smerilys@gmail.com>
2018-03-24 20:26:55 +0100

    Update Map.java

--- RPG/src/gameEngine/Map.java
+++ RPG/src/gameEngine/Map.java
@@ -9,16 +9,25 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import dataClasses.*;
+import java.awt.*;
 
 public class Map{
 	public Floor[] floors = new Floor[3];
 	
+	// Creation of the map
 	public Map() {
 		for(int i = 0; i < 3; i++) {
 			floors[i] = new Floor();
 		}
 	}
 	
+	// Returns the content of a cell
+	public int getContent(int floor, int room, int x, int y)
+	{
+		return floors[floor].rooms[room].roomLayout[x][y];
+	}
+	
+	// Debugging : print each floor separated by a line
 	public void printMap(Map map)
 	{
 		for(Floor f: floors)
