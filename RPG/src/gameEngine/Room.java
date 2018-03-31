c5a2340bce65f511eca50ffd09a881b9a16df841
Smerilys <smerilys@gmail.com>
2018-03-31 15:15:42 +0200

    Update Room.java

--- RPG/src/gameEngine/Room.java
+++ RPG/src/gameEngine/Room.java
@@ -11,7 +11,7 @@ import java.awt.Graphics;
 abstract class Room {
 	
 	// Array associating each type of square with an integral
-	public int [][]roomLayout = new int[16][15];
+	public Cell [][]roomLayout = new Cell[16][15];
 	
 	// Random number used for the creation of rooms randomly
 	protected Random randomNumber = new Random();
@@ -23,21 +23,30 @@ abstract class Room {
 		 * 0 = Wall
 		 * 1 = Obstacle
 		 * 2 = 'basic' floor
-		 * 3 = Exit (door to next room)
-		 * 4 = Shop
+		 * 3 = Exit (door to previous room)
+		 * 4 = Exit (door to next room)
+		 * 5 = Shop
 		 * 
 		 */
+		// Create every Cell 
+		for(int i = 0; i < 16; i++)
+		{
+			for(int j = 0; j < 15; j++)
+			{
+				roomLayout[i][j] = new Cell(2, null);
+			}
+		}
 		
 		// Initialize the borders as walls
 		for(int i = 0; i < 15; i++)
 		{
-			roomLayout[0][i] = 0;
-			roomLayout[15][i] = 0;
+			roomLayout[0][i].type = 0;
+			roomLayout[15][i].type = 0;
 		}
 		for(int i = 0; i < 16; i++)
 		{
-			roomLayout[i][0] = 0;
-			roomLayout[i][14] = 0;
+			roomLayout[i][0].type = 0;
+			roomLayout[i][14].type = 0;
 		}
 		
 		// Initialize the inside as basic floor
@@ -45,29 +54,28 @@ abstract class Room {
 		{
 			for(int j = 1; j < 15; j++)
 			{
-				roomLayout[j][i] = 2;
+				roomLayout[j][i].type = 2;
 			}
 		}
 		
 		// Placing the door 
 		int doorLocation = randomNumber.nextInt(4);
 		if(doorLocation == 0)
-			roomLayout[0][1+randomNumber.nextInt(12)] = 3;
+			roomLayout[0][1+randomNumber.nextInt(12)].type = 3;
 		else if (doorLocation == 1)
-			roomLayout[1+randomNumber.nextInt(11)][0] = 3;
+			roomLayout[1+randomNumber.nextInt(11)][0].type = 3;
 		else if (doorLocation == 2)
-			roomLayout[15][1+randomNumber.nextInt(12)] = 3;
+			roomLayout[15][1+randomNumber.nextInt(12)].type = 3;
 		else 
-			roomLayout[1+randomNumber.nextInt(11)][14] = 3;
+			roomLayout[1+randomNumber.nextInt(11)][14].type = 3;
 	}
 	
-	// Debugging : )rint the layout of a room
 	public void printLayout() {
 		for(int i = 0; i < 16; i++)
 		{
 			for(int j = 0; j < 15; j++)
 			{
-				System.out.print(roomLayout[i][j]);
+				System.out.print(roomLayout[i][j].type);
 				System.out.print(" ");
 			}
 			System.out.println(" ");
