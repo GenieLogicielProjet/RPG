a93aedbdd8fa620fb1dec4ca5bf02e0204c8c285
Smerilys <smerilys@gmail.com>
2018-03-31 15:14:28 +0200

    added cell.java to gameEngine

---
+++ RPG/src/gameEngine/Cell.java
@@ -0,0 +1,11 @@
+package gameEngine;
+
+public class Cell {
+	public int type;
+	public Object content;
+	
+	public Cell(int type, Object content) {
+		this.type = type;
+		this.content = content;
+	}
+}
