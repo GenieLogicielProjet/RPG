fb8ea1e08d3eae5a22bc85974ae8379b2249845f
Smerilys <smerilys@gmail.com>
2018-04-06 19:17:14 +0200

    Update Monster.java

--- RPG/src/players/Monster.java
+++ RPG/src/players/Monster.java
@@ -1,10 +1,36 @@
 package players;
 
 import dataClasses.*;
+import gameEngine.*;
 
-public abstract class Monster extends Player{
-	public Monster()
+public class Monster extends Player{	
+	private boolean present;
+	private Loot loot;
+	
+	public Monster(int swiftness, int armor, int agility, int strength, int power, int maxHealthPoints, boolean present)
 	{
-		super();
+		super(swiftness, armor, agility, strength, power, maxHealthPoints);
+		this.present = present;
+	}
+	
+	public void updateMonster(User player, Map map) {
+		if(this.getCurrentHealthPoints() <= 0) {
+			this.setPresent(false);
+		}
+		move(player, map);
+	}
+	
+	public void move(User player, Map map) {
+		Monster temp = new Monster(0, 0, 0, 0, 0, 0, false);
+		
+		
+	}
+	
+	public boolean isPresent() {
+		return present;
+	}
+
+	public void setPresent(boolean present) {
+		this.present = present;
 	}
 }
